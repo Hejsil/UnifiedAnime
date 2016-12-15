c152ae5ac0e846071dd21f1423f6be7600541bf8
Jimmi <Jimmi@DESKTOP-S6T6O2K>
2016-12-15 11:52:30 +0100

    Wrote more tests

--- UnifiedAnime.Tests/Browsers/AniListBrowserTests.cs
+++ UnifiedAnime.Tests/Browsers/AniListBrowserTests.cs
@@ -18,10 +18,11 @@ namespace UnifiedAnime.Tests.Browsers
             Browser.Authenticate(Resources.AniListClientId, Resources.AniListClientSecret);
         }
 
-        [Test()]
-        public void GetUserTest()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetUserTest(object displayName)
         {
-            var response = Browser.GetUser("UnifiedAnimeTestUser");
+            var response = Browser.GetUser(displayName.ToString());
             response.Status.ShouldBe(ResponseStatus.Success);
             response.Data.ShouldNotBeNull();
 
@@ -52,44 +53,11 @@ namespace UnifiedAnime.Tests.Browsers
             //user.MangaChap.ShouldBe(158); TODO: Don't know why this fails
         }
 
-        [Test()]
-        public void GetUserTest1()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetActivityTest(object displayName)
         {
-            var response = Browser.GetUser(84039);
-            response.Status.ShouldBe(ResponseStatus.Success);
-            response.Data.ShouldNotBeNull();
-
-            var user = response.Data;
-            user.About.ShouldBe("This is a test user for the library UnifiedAnime:\nhttps://github.com/Hejsil/UnifiedAnime");
-            user.AdultContent.ShouldBe(true);
-            user.AdvancedRating.ShouldBe(true);
-            user.AdvancedRatingNames.ShouldContain("TestRating1");
-            user.AdvancedRatingNames.ShouldContain("TestRating2");
-            user.AdvancedRatingNames.ShouldContain("TestRating3");
-            user.AnimeTime.ShouldBe(1571);
-            user.CustomListAnime.ShouldContain("TestCustomAnimeList1");
-            user.CustomListAnime.ShouldContain("TestCustomAnimeList2");
-            user.CustomListAnime.ShouldContain("TestCustomAnimeList3");
-            user.CustomListAnime.ShouldNotContain("TestCustomAnimeList4");
-            user.CustomListManga.ShouldContain("TestCustomMangaList1");
-            user.CustomListManga.ShouldContain("TestCustomMangaList2");
-            user.CustomListManga.ShouldContain("TestCustomMangaList3");
-            user.CustomListManga.ShouldNotContain("TestCustomMangaList4");
-            user.DisplayName.ShouldBe("UnifiedAnimeTestUser");
-            user.Id.ShouldBe(84039);
-            user.ImageUrlBanner.ShouldBe("https://cdn.anilist.co/img/dir/userbanner/84039-eOyAb7CU6LoL.png");
-            user.ImageUrlLge.ShouldBe("https://cdn.anilist.co/img/dir/user/reg/default.png");
-            user.ImageUrlMed.ShouldBe("https://cdn.anilist.co/img/dir/user/sml/default.png");
-            user.ListOrder.ShouldBe(ListOrder.Alphabet);
-            user.Notifications.ShouldBe(0);
-            user.ScoreType.ShouldBe(ScoreSystem.Star5);
-            //user.MangaChap.ShouldBe(158); TODO: Don't know why this fails
-        }
-
-        [Test()]
-        public void GetActivityTest()
-        {
-            var response = Browser.GetActivity("UnifiedAnimeTestUser");
+            var response = Browser.GetActivity(displayName.ToString());
             response.Status.ShouldBe(ResponseStatus.Success);
             response.Data.ShouldNotBeNull();
 
@@ -100,30 +68,11 @@ namespace UnifiedAnime.Tests.Browsers
             //       and i don't want to fix it every time it happens.
         }
 
-        [Test()]
-        public void GetActivityTest1()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetFollowersTest(object displayName)
         {
-            var response = Browser.GetActivity(84039);
-            response.Status.ShouldBe(ResponseStatus.Success);
-            response.Data.ShouldNotBeNull();
-
-            var activities = response.Data;
-            activities.Length.ShouldBe(16);
-
-            // TODO: This will be hard to test, because the activity might change all the time,
-            //       and i don't want to fix it every time it happens.
-        }
-
-        [Test()]
-        public void GetFollowersTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test()]
-        public void GetFollowersTest1()
-        {
-            var response = Browser.GetFollowers(84039);
+            var response = Browser.GetFollowers(displayName.ToString());
             response.Status.ShouldBe(ResponseStatus.Success);
             response.Data.ShouldNotBeNull();
 
@@ -135,56 +84,58 @@ namespace UnifiedAnime.Tests.Browsers
             follower.ImageUrlMed.ShouldBe("https://cdn.anilist.co/img/dir/user/reg/72340-aKJVAlZWuTRE.png");
         }
 
-        [Test()]
-        public void GetFollowingTest()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetFollowingTest(object displayName)
         {
-            Assert.Fail();
+            var response = Browser.GetFollowing(displayName.ToString());
+            response.Status.ShouldBe(ResponseStatus.Success);
+            response.Data.ShouldNotBeNull();
+
+            var followers = response.Data;
+            followers.Length.ShouldBe(1);
+            var follower = followers[0];
+            follower.DisplayName.ShouldBe("hejsil");
+            follower.ImageUrlLge.ShouldBe("https://cdn.anilist.co/img/dir/user/reg/72340-aKJVAlZWuTRE.png");
+            follower.ImageUrlMed.ShouldBe("https://cdn.anilist.co/img/dir/user/reg/72340-aKJVAlZWuTRE.png");
         }
 
-        [Test()]
-        public void GetFollowingTest1()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetFavouritesTest(object displayName)
         {
-            Assert.Fail();
-        }
+            var response = Browser.GetFavourites(displayName.ToString());
+            response.Status.ShouldBe(ResponseStatus.Success);
+            response.Data.ShouldNotBeNull();
 
-        [Test()]
-        public void GetFavouritesTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test()]
-        public void GetFavouritesTest1()
-        {
-            Assert.Fail();
+            // TODO: Make this when have the AniList account at hand
         }
 
         [Test()]
         public void SearchUserTest()
         {
-            Assert.Fail();
+            var response = Browser.SearchUser("UnifiedAnimeTestUser");
+            response.Status.ShouldBe(ResponseStatus.Success);
+            response.Data.ShouldNotBeNull();
+
+            var users = response.Data;
+            users.Length.ShouldBe(1);
+            var user = users[0];
+            user.DisplayName.ShouldBe("UnifiedAnimeTestUser");
+            user.ImageUrlLge.ShouldBe("https://cdn.anilist.co/img/dir/user/reg/default.png");
+            user.ImageUrlMed.ShouldBe("https://cdn.anilist.co/img/dir/user/sml/default.png");
         }
 
-        [Test()]
-        public void GetAnimelistTest()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetAnimelistTest(object displayName)
         {
             Assert.Fail();
         }
 
-        [Test()]
-        public void GetAnimelistTest1()
-        {
-            Assert.Fail();
-        }
-
-        [Test()]
-        public void GetMangalistTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test()]
-        public void GetMangalistTest1()
+        [TestCase("UnifiedAnimeTestUser")]
+        [TestCase(84039)]
+        public void GetMangalistTest(object displayName)
         {
             Assert.Fail();
         }
